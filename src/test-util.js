4de69fc80fed991ebf5d121a205df64ff4b84de1
Buhiire Keneth <buhiirekeneth@gmail.com>
2022-04-30 16:44:02 +0300

    Add test utils for testing redux components

---
+++ src/test-util.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { render as rtlRender } from "@testing-library/react";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
+import userReducer from "redux/user";
+
+function render(
+  ui,
+  {
+    preloadedState,
+    store = configureStore({ reducer: { user: userReducer }, preloadedState }),
+    ...renderOptions
+  } = {}
+) {
+  function Wrapper({ children }) {
+    return <Provider store={store}>{children}</Provider>;
+  }
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+}
+
+export * from "@testing-library/react";
+
+export { render };
